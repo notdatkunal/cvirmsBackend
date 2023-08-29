a076998d44d972e090c3e10ff31547dd09b6c2fc
Kunal Dabhane <kunaldabhane20012@gmail.com>
2023-08-29 07:36:13 +0530

    member controller added

--- src/main/java/com/application/cvirms/controller/MemberController.java
+++ src/main/java/com/application/cvirms/controller/MemberController.java
@@ -1,16 +1,56 @@
 package com.application.cvirms.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.application.cvirms.dto.features.Emergency;
+import com.application.cvirms.dto.features.Notice;
+import com.application.cvirms.dto.member.Visitor;
+import com.application.cvirms.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/member")
 public class MemberController {
 
+//    @GetMapping
+//    public Guest dashboard(){
+//        return new Guest();
+//    }
+
+
+
+    @Autowired
+    @Qualifier("tenantMemberService")
+    public MemberService service;
     @GetMapping
-    public String dashboard(){
-        return "this is tenant";
+    public List<Visitor> getVisitors(@PathVariable(required = true) Integer memberId){
+        return service.getVisitorsById(memberId);
     }
+
+    @PostMapping("/add")
+    public ResponseEntity addVisitor(@Validated Visitor visitor){
+        return service.addVisitor(visitor);
+    }
+    @GetMapping("/notices")
+    public ResponseEntity<List<Notice>> getAllNotices(){
+        return service.getAllNotices();
+    }
+
+    @GetMapping("/emergency")
+    public ResponseEntity<List<Emergency>> getEmergency(Emergency emergency){
+        return service.getEmergency(emergency);
+    }
+    @PostMapping("/emergency/{memberId}")
+    public ResponseEntity addEmergency(@PathVariable Integer memberId){
+        return service.addEmergency(memberId);
+    }
+
+
+
 
 }

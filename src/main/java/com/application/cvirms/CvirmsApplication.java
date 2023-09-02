4647e5ac945a32dbefbfbe51b6443d39e8f2c739
Kunal Dabhane <kunaldabhane20012@gmail.com>
2023-09-02 13:02:47 +0530

    added configuration changes to app loading

--- src/main/java/com/application/cvirms/CvirmsApplication.java
+++ src/main/java/com/application/cvirms/CvirmsApplication.java
@@ -1,9 +1,12 @@
 package com.application.cvirms;
 
+import com.application.cvirms.config.AppConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(AppConfig.class)
 public class CvirmsApplication {
 
 	public static void main(String[] args) {

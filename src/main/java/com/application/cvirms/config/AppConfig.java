a10a7ccfdb2b1c50035f1d160e357938110754a9
Kunal Dabhane <kunaldabhane20012@gmail.com>
2023-09-02 13:02:26 +0530

    configuration changes

--- src/main/java/com/application/cvirms/config/AppConfig.java
+++ src/main/java/com/application/cvirms/config/AppConfig.java
@@ -1,4 +1,10 @@
 package com.application.cvirms.config;
 
+import com.application.cvirms.dto.member.Status;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AppConfig {
+
 }
